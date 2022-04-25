5c00e0f6fb7698e81d6cb07d2864ce2df2c1a3c8
Esther Cho <92740628+helloecho12345@users.noreply.github.com>
2022-04-25 17:13:41 +0100

    dice roller rolls 2 dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,6 +1,12 @@
-def main():
-  roll = 5
-  print(f'You rolled a {roll}')
+import random
 
-if __name__== "__main__":
-  main()
+dice_rolls = 2
+
+for i in range(0, dice_rolls):   # for every index in a range starting after 0 until the value is equal to dice_rolls, do this:
+  
+  def main():
+    roll = random.randint(1,6)   # range is inclusive
+    print(f'You rolled a {roll}')
+
+  if __name__== "__main__":
+    main()
