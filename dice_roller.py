6f86f3658509be212544e84e308d3932d694407c
Esther Cho <92740628+helloecho12345@users.noreply.github.com>
2022-04-25 17:28:48 +0100

    Tutorial complete

--- dice_roller.py
+++ dice_roller.py
@@ -8,7 +8,13 @@ for i in range(0, dice_rolls):   # for every index in a range starting after 0 u
   # def main():
   roll = random.randint(1,6)   # range is inclusive
   dice_sum += roll   # same as dice_sum = dice_sum + roll
-  print(f'You rolled a {roll}')
+  if roll == 1:
+    print(f'You rolled a {roll}! Critical Fail')
+  elif roll == 6:
+    print(f'You rolled a {roll}! Critical Success!')
+  else:
+    print(f'You rolled a {roll}')
+
 print(f'You have rolled a total of {dice_sum}')
 
   # if __name__== "__main__":
