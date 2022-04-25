1b86e70b2ed18560052f4061d9fad1be5a89b466
Esther Cho <92740628+helloecho12345@users.noreply.github.com>
2022-04-25 17:21:10 +0100

    Now rollng two dice and stating total of dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,12 +1,15 @@
 import random
 
 dice_rolls = 2
+dice_sum = 0
 
 for i in range(0, dice_rolls):   # for every index in a range starting after 0 until the value is equal to dice_rolls, do this:
   
-  def main():
-    roll = random.randint(1,6)   # range is inclusive
-    print(f'You rolled a {roll}')
+  # def main():
+  roll = random.randint(1,6)   # range is inclusive
+  dice_sum += roll   # same as dice_sum = dice_sum + roll
+  print(f'You rolled a {roll}')
+print(f'You have rolled a total of {dice_sum}')
 
-  if __name__== "__main__":
-    main()
+  # if __name__== "__main__":
+  #   main()
